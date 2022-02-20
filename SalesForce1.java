8f309b08ae2069c0bdfbd71d33ef24a7aa17cfff
Akas Lk <99659066+LKAKASH@users.noreply.github.com>
2022-02-20 20:33:18 +0530

    Add files via upload

--- SalesForce1.java
+++ SalesForce1.java
@@ -15,21 +15,37 @@ public class SalesForce1 {
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get(" https://login.salesforce.com");
+		driver.get("https://qeagle-dev-ed.my.salesforce.com/");
+		
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("hari.radhakrishnan@qeagle.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password@123");
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+		
+		driver.findElement(By.xpath("(//div[@data-aura-class='uiTooltip'])[9]")).click();
+		driver.findElement(By.xpath("//button[@aria-label='View All Applications']")).click();
+		
+		
+		driver.findElement(By.xpath("(//p[@class='slds-truncate'])[7]")).click();
+		driver.findElement(By.xpath("(//one-app-nav-bar-item-root[@data-assistive-id='operationId'])[2]")).click();
+		
+		driver.findElement(By.xpath("//a[@title='New']")).click();
+		
+		driver.findElement(By.xpath("//input[@name='Name']")).sendKeys("Salesforce Automation by Akash");
+		driver.findElement(By.xpath("//input[@name='CloseDate']")).sendKeys("8/19/2022");
+		driver.findElement(By.xpath("(//button[@aria-haspopup='listbox'])[1]")).click();
+		driver.findElement(By.xpath("//button[@aria-activedescendant='combobox-button-226-3-226']")).click();
+		
+		driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+		
+		
+		
+		driver.close();
+		
+		
+		
+		
 
-		driver.findElement(By.id("username")).sendKeys("akash");
-		driver.findElement(By.id("password")).sendKeys("lusukutty192919");
-		driver.findElement(By.id("Login")).click();
-		driver.findElement(By.id("rememberUn")).click();
-
-		String text = driver.findElement(By.xpath("//div[@id='error']")).getText();
-		String text1 = "Please check your username and password. If you still can't log in, contact your Salesforce administrator.";
-
-		if (text.equals(text1)) {
-			System.out.println("Test case pass");
-		} else {
-			System.out.println("Test case not pass");
-		}
+		
 
 	}
 
