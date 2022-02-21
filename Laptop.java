513abc2b2670b9492890b88d3e3e0a07225f52b7
Akas Lk <99659066+LKAKASH@users.noreply.github.com>
2022-02-21 21:39:42 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,9 @@
+package week3.day1;
+
+public class Laptop {
+	public void getSystemInfo() {
+		System.out.println("Windows 10");
+	}
+}
+
+
---
+++ Sports.java
@@ -0,0 +1,31 @@
+package week3.day1;
+
+
+public class Sports {
+	public void acheiver(String  name,int medalCount) {
+		System.out.println("Name is: "+name);
+		System.out.println("Medal Count:"+medalCount);
+	}
+	public void acheiver(String country ,String sportName, int rank) {
+		System.out.println("Country name: "+country);
+		System.out.println("Game name is:"+sportName);
+		System.out.println("Rank:"+rank);
+	}
+	public void acheiver(float weight, int height, long contactNo) {
+		System.out.println("Weight:"+weight);
+		System.out.println("Height:"+height);
+		System.out.println("ContactNo:"+contactNo);
+	}
+	public void acheiver(String tournamentName, long prizeMoney) {
+		System.out.println("Tournament Name:"+tournamentName);
+		System.out.println("Prize Money:"+prizeMoney);
+	}
+	public static void main(String[] args) {
+		Sports get=new Sports();
+		get.acheiver("Akash R", 1);
+		get.acheiver("India", "Track Events", 2);
+		get.acheiver(58, 176, 90898989);
+		get.acheiver("National 2022 Trackevents", 140000);
+	}
+}
+
